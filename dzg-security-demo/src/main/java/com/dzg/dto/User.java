a0d60e2c1960bedbdb7702690b29ac86936618e1
dzg <1395224335@qq.com>
2019-01-30 23:55:18 +0800

    user实体类

--- dzg-security-demo/src/main/java/com/dzg/dto/User.java
+++ dzg-security-demo/src/main/java/com/dzg/dto/User.java
@@ -1,7 +1,12 @@
 package com.dzg.dto;
 
+import com.dzg.validator.MyConstraint;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import java.util.Date;
 
 /**
  * @program: dzg-security
@@ -14,8 +19,15 @@ public class User  {
     public interface UserSimpleView {};
     public interface UserDetailView extends UserSimpleView {};
     @JsonView(UserSimpleView.class)
+    private Integer id;
+    @JsonView(UserSimpleView.class)
+    @MyConstraint(message = "这是dzg第一个自定义校验注解")
     private String username;
     @JsonView(UserDetailView.class)
+    @NotBlank(message = "密码不能为空")
     private String password;
+    @Past(message = "生日必须是过去的时间")
+    @JsonView(UserSimpleView.class)
+    private Date birthday;
 
 }
